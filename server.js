111fef9b34706950c53e694e42aeddab41afc19b
Marie S <marieslo@Maries.local>
2024-02-29 16:30:51 +0200

    changing deploy resource

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const connectToDatabase = require('./database'); 
 
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const petRoutes = require('./routes/petRoutes');
+const authRoutes = require('./routes/authRoutes')
+const userRoutes = require('./routes/userRoutes')
+const petRoutes = require('./routes/petRoutes')
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors({ credentials: true }));
 app.use(express.json());
@@ -15,10 +16,12 @@ app.get('/', (req, res) => {
   res.send('Backend running');
 });
 
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/pets', petRoutes);
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+app.use('/pets', petRoutes);
 
 connectToDatabase(); 
 
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
